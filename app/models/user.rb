8daad3f43ae183eff47498968f27564eba4d8cd1
RiddickAngiela <angielariddick@gmail.com>
2023-06-06 14:16:59 +0300

    working on instagram clone

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+            
+         has_many :posts
+
+          def randomize_id
+    begin
+    self.id = SecureRandom.random_number(1_000_000_000)
+    end while User.where(id: self.id).exists?
+    end
+
+          enum role: [:user, :admin]
+          after_initialize :set_default_role, :if => :new_record?
+          def set_default_role
+          self.role ||= :admin
+          end
+end
